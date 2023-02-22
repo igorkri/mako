4649adce951727a62b99202d896b65ee8728d231
igor <igorkri26@gmail.com>
2023-02-23 00:33:03 +0200

    dev button

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -28,10 +28,10 @@
             echo \hail812\adminlte\widgets\Menu::widget([
                 'items' => [
 //                    ['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
-                    ['label' => 'Yii2 PROVIDED', 'header' => true],
-                    ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
-                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
-                    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
+//                    ['label' => 'Yii2 PROVIDED', 'header' => true],
+//                    ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
+//                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
+//                    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
 
                     ['label' => 'Сайт', 'header' => true],
                     ['label' => 'Замовлені дзвінки', 'icon' => 'dot-circle', 'url' => ['/order-a-call']],
