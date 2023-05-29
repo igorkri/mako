8a3c568f66bfe5c30bc5b981ee1d601e5dee82ed
Olexiy-css <dianoboll@gmail.com>
2023-05-29 10:32:44 +0300

    google fonts

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -17,6 +17,9 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="/img/favicon.svg" type="image/x-icon">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
